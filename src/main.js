658d22b7068ecae034e7b8e5906b6a5177940341
Faye <faye@fayevr.dev>
2024-08-21 23:00:43 +0200

    update

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { Client, Databases, Account } from "node-appwrite";
-import { getEvents } from "./utils/event-actions.js";
+import { getUpcomingEvents } from "./utils/event-actions.js";
 
 export const client = new Client();
 export const account = new Account(client);
@@ -9,22 +9,21 @@ export const databases = new Databases(client);
 export { ID } from "node-appwrite";
 
 export default async ({ req, res, log, error }) => {
+  const userId = req.headers["x-appwrite-user-id"];
+
   client
     .setEndpoint(`${process.env.APPWRITE_API_URL}/v1`)
     .setProject(`${process.env.APPWRITE_PROJECT_ID}`)
-    .setJWT(req.headers["x-appwrite-user-jwt"] || "");
-
-  const userId = req.headers["x-appwrite-user-id"];
+    .setJWT(userId || "");
 
   if (req.method === "GET") {
-    log(req);
     switch (req.path) {
-      case "/getEvents":
-        const userEvents = await getEvents();
-        if (!userEvents) {
+      case "/getUpcomingEvents":
+        const upcomingEvents = await getUpcomingEvents();
+        if (!upcomingEvents) {
           return res.send("Error fetching user data");
         }
-        return res.json(userEvents);
+        return res.json(upcomingEvents);
       default:
         return res.json("No peeking.");
     }
--- src/utils/event-actions.js
+++ src/utils/event-actions.js
@@ -1,5 +1,15 @@
-import { databases } from "../main.js";
+import { databases } from '../main.js'
+import { Query } from 'node-appwrite'
 
-export async function getEvents() {
-  return await databases.listDocuments("hp_db", "events");
+export async function getUpcomingEvents() {
+  const data = await databases.listDocuments('hp_db', 'events', [
+    Query.orderAsc('date'),
+  ])
+
+  const currentDate = new Date()
+
+  return data.documents.filter(event => {
+    const eventDateUntil = new Date(event.dateUntil)
+    return eventDateUntil > currentDate
+  })
 }
