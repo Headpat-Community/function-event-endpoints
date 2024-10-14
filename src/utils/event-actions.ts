c34ba90863a3c3fa6c2b4f4ab4533983d7efe8c4
Faye <faye@fayevr.dev>
2024-10-14 23:00:50 +0200

    add 0

--- src/utils/event-actions.ts
+++ src/utils/event-actions.ts
@@ -1,14 +1,15 @@
-import {databases, databasesAdmin} from "../main.js";
-import {ID, Permission, Query, Role} from "node-appwrite";
-import {checkAuthentication, handleResponse} from "./errorHandler.js";
+import { databases, databasesAdmin } from "../main.js";
+import { ID, Permission, Query, Role } from "node-appwrite";
+import { checkAuthentication, handleResponse } from "./errorHandler.js";
 
 export async function getEvent(query: { eventId: string }, error: any) {
   try {
     const event = await databases.getDocument("hp_db", "events", query.eventId);
-    const attendees = await databasesAdmin.listDocuments("hp_db", "events-attendees", [
-      Query.equal("eventId", query.eventId),
-      Query.limit(50000000)
-    ]);
+    const attendees = await databasesAdmin.listDocuments(
+      "hp_db",
+      "events-attendees",
+      [Query.equal("eventId", query.eventId), Query.limit(50000000)],
+    );
     return { ...event, attendees: attendees.total };
   } catch (e) {
     error("Error fetching event", e);
@@ -16,12 +17,16 @@ export async function getEvent(query: { eventId: string }, error: any) {
   }
 }
 
-export async function getEventAttendees(query: { eventId: string }, error: any) {
+export async function getEventAttendees(
+  query: { eventId: string },
+  error: any,
+) {
   try {
-    const attendees = await databasesAdmin.listDocuments("hp_db", "events-attendees", [
-      Query.equal("eventId", query.eventId),
-      Query.limit(50000000)
-    ]);
+    const attendees = await databasesAdmin.listDocuments(
+      "hp_db",
+      "events-attendees",
+      [Query.equal("eventId", query.eventId), Query.limit(50000000)],
+    );
     return attendees.total;
   } catch (e) {
     error("Error fetching event attendees", e);
@@ -42,10 +47,20 @@ export async function getNextEvent(error: any) {
       return data.documents;
     }
 
-    return data.documents.filter((event) => {
+    const nextEvent = data.documents.filter((event) => {
       const eventDateUntil = new Date(event.dateUntil);
       return eventDateUntil > currentDate;
     })[0];
+
+    if (nextEvent) {
+      const attendees = await getEventAttendees(
+        { eventId: nextEvent.$id },
+        error,
+      );
+      return { ...nextEvent, attendees: attendees };
+    }
+
+    return nextEvent;
   } catch (e) {
     return handleResponse(
       "Error fetching next events",
@@ -55,7 +70,10 @@ export async function getNextEvent(error: any) {
   }
 }
 
-export async function getEvents(query: { offset: number, limit: number }, error: any) {
+export async function getEvents(
+  query: { offset: number; limit: number },
+  error: any,
+) {
   const currentDate = new Date();
 
   try {
@@ -67,13 +85,18 @@ export async function getEvents(query: { offset: number, limit: number }, error:
       Query.offset(query.offset || 0),
     ]);
 
-    const eventsWithAttendees = await Promise.all(data.documents.map(async (event) => {
-      const attendees = await getEventAttendees({ eventId: event.$id }, error);
-      return { ...event, attendees: attendees };
-    }));
+    const eventsWithAttendees = await Promise.all(
+      data.documents.map(async (event) => {
+        const attendees = await getEventAttendees(
+          { eventId: event.$id },
+          error,
+        );
+        return { ...event, attendees: attendees || 0 };
+      }),
+    );
 
     return eventsWithAttendees.filter((event) => {
-      const eventDateUntil = new Date(event['dateUntil']);
+      const eventDateUntil = new Date(event["dateUntil"]);
       return eventDateUntil > currentDate;
     });
   } catch (e) {
@@ -97,7 +120,11 @@ export async function getUpcomingEvents(error: any) {
     });
   } catch (e) {
     error("Error fetching upcoming events", e);
-    return handleResponse("Error fetching upcoming events", "events_upcoming_fetch-error", 500);
+    return handleResponse(
+      "Error fetching upcoming events",
+      "events_upcoming_fetch-error",
+      500,
+    );
   }
 }
 
@@ -113,24 +140,42 @@ export async function getArchivedEvents(error: any) {
     return data.documents;
   } catch (e) {
     error("Error fetching archived events", e);
-    return handleResponse("Error fetching archived events", "events_archived_fetch-error", 500);
+    return handleResponse(
+      "Error fetching archived events",
+      "events_archived_fetch-error",
+      500,
+    );
   }
 }
 
-export async function postEventAttendee(userId: string, query: { eventId: string }, error: any) {
+export async function postEventAttendee(
+  userId: string,
+  query: { eventId: string },
+  error: any,
+) {
   await checkAuthentication(userId);
 
   try {
-    await databasesAdmin.createDocument("hp_db", "events-attendees", ID.unique(), {
-      eventId: query.eventId,
-      userId: userId,
-    }, [
-      Permission.read(Role.user(userId)),
-      Permission.delete(Role.user(userId))
-    ]);
+    await databasesAdmin.createDocument(
+      "hp_db",
+      "events-attendees",
+      ID.unique(),
+      {
+        eventId: query.eventId,
+        userId: userId,
+      },
+      [
+        Permission.read(Role.user(userId)),
+        Permission.delete(Role.user(userId)),
+      ],
+    );
     return handleResponse("Attendee added", "event-attendee-add-success", 200);
   } catch (e) {
     error("Error adding attendee", e);
-    return handleResponse("Error adding attendee", "event-attendee-add-error", 500);
+    return handleResponse(
+      "Error adding attendee",
+      "event-attendee-add-error",
+      500,
+    );
   }
 }
